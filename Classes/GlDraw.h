0349a1216d127ab9827e1ccf4dce195002583e28
Blake <blakejia@gmail.com>
2016-03-23 23:05:33 +0800

    change HelloWorldScene

--- Classes/GlDraw.h
+++ Classes/GlDraw.h
@@ -1,20 +1,20 @@
-#ifndef _HelloWorld_H_
-#define _HelloWorld_H_
+#ifndef _GlDraw_H_
+#define _GlDraw_H_
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::Layer
+class GlDraw : public cocos2d::Layer
 {
 public:
-    static cocos2d::Scene* createScene();
-    
-    virtual bool init();
-    
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+ 
     virtual void visit(cocos2d::Renderer *renderer, const cocos2d::Mat4& transform, uint32_t parentFlags) override;
-    
+
     void onDraw();
-    
-    CREATE_FUNC(HelloWorld);
+	
+    CREATE_FUNC(GlDraw);
 private:
     cocos2d::CustomCommand _command;
 };
--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -2,85 +2,65 @@
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+CCScene* HelloWorld::createScene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
+    auto s = Scene::create();
     
-    // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
-
-    // add layer as a child to scene
-    scene->addChild(layer);
-
-    // return the scene
-    return scene;
+    
+    s->addChild(layer);
+    
+    return s;
 }
 
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
     if ( !Layer::init() )
     {
         return false;
     }
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    this->setGLProgram(GLProgramCache::getInstance()->getGLProgram(GLProgram::SHADER_NAME_POSITION_COLOR));
     
     return true;
 }
 
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void HelloWorld::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4& transform, uint32_t parentFlags)
 {
-    Director::getInstance()->end();
+    Layer::visit(renderer, transform, parentFlags);
+    _command.init(_globalZOrder);
+    _command.func = CC_CALLBACK_0(HelloWorld::onDraw, this);
+    Director::getInstance()->getRenderer()->addCommand(&_command);
+}
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+void HelloWorld::onDraw()
+{
+    //获得当前HelloWorld的shader
+    auto glProgram = getGLProgram();
+    //使用此shader
+    glProgram->use();
+    //设置该shader的一些内置uniform,主要是MVP，即model-view-project矩阵
+    glProgram->setUniformsForBuiltins();
+    
+    auto size = Director::getInstance()->getWinSize();
+    //指定将要绘制的三角形的三个顶点，分别位到屏幕左下角，右下角和正中间的顶端
+    float vertercies[] = { 0,0,   //第一个点的坐标
+        size.width, 0,   //第二个点的坐标
+        size.width / 2, size.height };  //第三个点的坐标
+    //指定每一个顶点的颜色，颜色值是RGBA格式的，取值范围是0-1
+    float color[] = { 0, 1,0, 1,    //第一个点的颜色，绿色
+        1,0,0, 1,  //第二个点的颜色, 红色
+        0, 0, 1, 1 };  //第三个点的颜色， 蓝色
+    //激活名字为position和color的vertex attribute
+    GL::enableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION | GL::VERTEX_ATTRIB_FLAG_COLOR);
+    //分别给position和color指定数据源
+    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, vertercies);
+    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, color);
+    //绘制三角形，所谓的draw call就是指这个函数调用
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //glPolygonMode(GL_TRIANGLES, 3, false);
+    //通知cocos2d-x 的renderer，让它在合适的时候调用这些OpenGL命令
+    CC_INCREMENT_GL_DRAWN_BATCHES_AND_VERTICES(1, 3);
+    //如果出错了，可以使用这个函数来获取出错信息
+    CHECK_GL_ERROR_DEBUG();
 }
